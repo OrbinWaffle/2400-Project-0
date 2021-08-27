3aee55e95a4d25a75bf90f7b202e5fbb020891d3
OrbinWaffle <79740022+OrbinWaffle@users.noreply.github.com>
2021-08-26 17:50:01 -0700

    Update Calculator.java
    fixed broken program

--- Calculator.java
+++ Calculator.java
@@ -9,6 +9,47 @@ public class Calculator
 
    public Calculator()
    {
+      result = 0.0f;
+   }
+   public double getResult()
+   {
+      return result;
+   }
+   public void doCalculation(String expression)
+                            throws UnknownOperatorException,
+                                   NumberFormatException
+   {
+      Character operator = null;
+      double number = 0;
+      int index = 0;
+      for(index = 0; index < expression.length(); index++)
+      {
+         if(operator == null
+            && !Character.isWhitespace(expression.charAt(index)))
+         {
+            operator = expression.charAt(index);
+            break;
+         }
+      }
+      if (operator != '+' && operator != '-'
+          && operator != '*'&& operator != '/')
+      {
+         throw new UnknownOperatorException(operator);
+      }
+      number = Double.parseDouble(expression.substring(index + 1));
+      if(operator == '+')
+      {
+         result += number;
+      }
+      else if (operator == '-')
+      {
+         result -= number;
+      }
+      else if (operator == '*')
+      {
+         result *= number;
+      }
+      else if (operator == '/')
       {
          result /= number;
       }
